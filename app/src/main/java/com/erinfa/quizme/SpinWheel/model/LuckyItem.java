e42cc0cc9f2909a8a1ccb883e999a2bd827daa1f
VIPIN KUSHWAHA <102202599+kushwahavipin@users.noreply.github.com>
2022-05-27 13:06:04 +0530

    update

---
+++ app/src/main/java/com/erinfa/quizme/SpinWheel/model/LuckyItem.java
@@ -0,0 +1,11 @@
+package com.erinfa.quizme.SpinWheel.model;
+
+
+public class LuckyItem {
+    public String topText;
+    public String secondaryText;
+    public int secondaryTextOrientation;
+    public int icon;
+    public int color;
+    public int textColor;
+}
